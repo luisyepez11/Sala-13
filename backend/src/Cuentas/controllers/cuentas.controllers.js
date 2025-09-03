4bc31bce76ed9fdc170906ae96e055088d8cb2ba
luisyepez11 <luisrobert11660@gmail.com>
2025-09-03 02:23:10 -0400

    carga de datos

--- backend/src/Cuentas/controllers/cuentas.controllers.js
+++ backend/src/Cuentas/controllers/cuentas.controllers.js
@@ -3,7 +3,30 @@ import { pool } from "../../db.js";
 export const getCuenta= async(req,res) =>{
     try {
         const idCuenta = req.params.id;
-        const [resultCuenta] = await pool.query(`SELECT * FROM cuentas where idcuenta = ?`,[idCuenta])
+        const [resultCuenta] = await pool.query(`
+            SELECT 
+    cu.idcuenta,
+    cu.*,
+    COUNT(DISTINCT CASE WHEN s.estado = 'pendiente' THEN s.idsolicitudes END) as total_solicitudes,
+    COUNT(DISTINCT ase.idamigos) as total_seguidos,
+    COUNT(DISTINCT aseg.idamigos) as total_seguidores,
+    COUNT(DISTINCT c.idcomentario) as total_comentarios
+FROM 
+    cuentas cu
+LEFT JOIN 
+    solicitudes s ON s.idReceptor = cu.idcuenta AND s.estado = 'pendiente'
+LEFT JOIN 
+    amigos ase ON ase.idUsuario = cu.idcuenta
+LEFT JOIN 
+    amigos aseg ON aseg.idReceptor = cu.idcuenta
+LEFT JOIN 
+    comentarios c ON c.idCuenta = cu.idcuenta
+WHERE cu.idcuenta=?
+GROUP BY 
+    cu.idcuenta
+ORDER BY 
+    total_solicitudes DESC;
+            `,[idCuenta])
         res.status(201).json({resultCuenta})
     } catch (error) {
         res.json({
