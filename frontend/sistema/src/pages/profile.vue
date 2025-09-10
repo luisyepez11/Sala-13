23dfb14dae6713b851c877fb8b80626370d3a042
Guaik <gpinzones23@ujap.edu.ve>
2025-09-10 12:14:15 -0400

    Implementacion de la creacion de comunidades desde el perfil del usuario

--- frontend/sistema/src/pages/profile.vue
+++ frontend/sistema/src/pages/profile.vue
@@ -226,11 +226,18 @@
 		}
 	}
 	const modalCrearListas = ref(false);
-	const abrirModal = () => {
+	const abrirModalListas = () => {
 		modalCrearListas.value = true
 	}
-	const cerrarModal = () => {
+	const cerrarModalLista = () => {
 		modalCrearListas.value = false
+	}
+	const modalCrearComunidades = ref(false);
+	const abrirModalComunidades = () => {
+		modalCrearComunidades.value = true
+	}
+	const cerrarModalComunidades = () => {
+		modalCrearComunidades.value = false
 	}
 	const crearLista = async () => {
 		try {
@@ -247,6 +254,7 @@
 			console.error("Error creando lista:", error);
 		}
 	}
+	
 	const buscar = (nombre)=>{
 		if (nombre==""){
 			router.push("/")
@@ -272,7 +280,7 @@
 </script>
 
 <template>
-	<Modal :isOpen="modalCrearListas" @close="cerrarModal">
+	<Modal :isOpen="modalCrearListas" @close="cerrarModalLista">
 		<div class="modal-crear-lista">
 			<h2 class="modal-title">Crear nueva lista</h2>
 			<div class="form-group">
@@ -286,8 +294,27 @@
 					class="input-field textarea"></textarea>
 			</div>
 			<div class="modal-btn">
-				<button class="btn-cancel" @click="cerrarModal">Cancelar</button>
+				<button class="btn-cancel" @click="cerrarModalLista">Cancelar</button>
 				<button class="btn-crear" @click="crearLista">Crear</button>
+			</div>
+		</div>
+	</Modal>
+	<Modal :isOpen="modalCrearComunidades" @close="cerrarModalComunidades">
+		<div class="modal-crear-lista">
+			<h2 class="modal-title">Crear nueva Comunidad</h2>
+			<div class="form-group">
+				<label class="user-bio" for="nombreLista">Nombre de la Comunidad</label>
+				<input id="nombreLista" type="text" v-model="nombreLista" placeholder="Ejemplo: Fanaticos del Cine"
+					class="input-field" />
+			</div>
+			<div class="form-group">
+				<label class="user-bio" for="descripcion">Descripción</label>
+				<textarea id="descripcion" v-model="descripcion" placeholder="Describe tu comunidad..."
+					class="input-field textarea"></textarea>
+			</div>
+			<div class="modal-btn">
+				<button class="btn-cancel" @click="cerrarModalComunidades">Cancelar</button>
+				<button class="btn-crear" @click="">Crear</button>
 			</div>
 		</div>
 	</Modal>
@@ -422,7 +449,7 @@
 
 			<div v-else-if="activeTab === 'Listas'" class="content-area">
 				<div v-if="!lista || lista.length === 0" class="listas-vacias">
-					<div class="lista-card create-card" @click="abrirModal">
+					<div class="lista-card create-card" @click="abrirModalListas">
 						<div class="lista-info">
 							<h2 class="lista-title">+ Crear nueva lista</h2>
 							<p class="lista-description">Empieza a organizar tus películas</p>
@@ -440,7 +467,7 @@
 							<p class="lista-description">{{ item.descripcion }}</p>
 						</div>
 					</div>
-					<div class="lista-card create-card" @click="abrirModal">
+					<div class="lista-card create-card" @click="abrirModalListas">
 						<div class="lista-info">
 							<h2 class="lista-title">+ Crear nueva lista</h2>
 							<p class="lista-description">Empieza a organizar tus películas</p>
@@ -455,17 +482,33 @@
 				</div>
 			</div>
 			<div v-else-if="activeTab === 'Comunidades'" class="content-area">
-  				<div class="comunidades-grid">
-    				<CommunityCard
-      					v-for="comunidad in comunidades"
-      					:key="comunidad.id"
-      					:titulo="comunidad.titulo"
-      					:descripcion="comunidad.descripcion"
-      					:imagen="comunidad.imagen"
-     					:usuarios="comunidad.usuarios"
-    				/>
-  				</div>
+				<div v-if="!comunidades || comunidades.length === 0" class="comunidades-vacias">
+					<div class="lista-card create-card" @click="abrirModalComunidades">
+						<div class="lista-info">
+							<h2 class="lista-title">+ Crear nueva comunidad</h2>
+							<p class="lista-description">Empieza a construir tu espacio cinéfilo</p>
+						</div>
+					</div>
 				</div>
+				<div v-else class="comunidades-grid">
+					<CommunityCard
+						v-for="comunidad in comunidades"
+						:key="comunidad.id"
+						:titulo="comunidad.titulo"
+						:descripcion="comunidad.descripcion"
+						:imagen="comunidad.imagen"
+						:usuarios="comunidad.usuarios"
+					/>
+
+					<div class="lista-card create-card" @click="abrirModalComunidades">
+						<div class="lista-info">
+							<h2 class="lista-title">+ Crear nueva comunidad</h2>
+							<p class="lista-description">Empieza a construir tu espacio cinéfilo</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
 			<div v-else class="empty-content">
 				<div class="empty-text">Contenido de {{ activeTab }} próximamente...</div>
 			</div>
@@ -591,7 +634,7 @@
 	.lista-title {
 		font-size: 1.25rem;
 		margin-bottom: 0.5rem;
-		text-align: left;
+		text-align: center;
 	}
 
 	.lista-description {
@@ -601,6 +644,8 @@
 	}
 
 	.create-card {
+		height: 565px;
+		width: 270px;
 		background: rgba(75, 85, 99, 0.3);
 		border: 2px dashed #6b7280;
 		display: flex;
