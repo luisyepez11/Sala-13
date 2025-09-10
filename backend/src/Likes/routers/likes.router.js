d882a8f70cf4f5cdc85015ee06ca5685da3d1d71
luisyepez11 <luisrobert11660@gmail.com>
2025-09-10 06:42:51 -0400

    likes

--- backend/src/Likes/routers/likes.router.js
+++ backend/src/Likes/routers/likes.router.js
@@ -1,10 +1,9 @@
 import { Router } from 'express';
-import {insertComentario,getComentarios,getComentariosLanding} from '../controllers/likes.controllers.js';
+import {insertLikes} from '../controllers/likes.controllers.js';
 
 const router = Router();
 
-router.post("/",insertComentario)
-router.get('/pelicula/:id',getComentarios)
-router.get("/comentarios",getComentariosLanding)
+router.post("/",insertLikes)
+
 
 export default router;
