144b683abce1ace799dc0a8c63b6ac768c105d54
BratPython <alejandro.lopez070605@gmail.com>
2025-09-12 02:44:38 -0400

    solucion de un bug ups

--- frontend/sistema/src/router/index.js
+++ frontend/sistema/src/router/index.js
@@ -15,6 +15,7 @@ import PdfViewer from "../pages/PdfViewer.vue";
 import ContactPage from "../pages/ContactPage.vue";
 import Comunidades from "./../pages/Comunidades.vue"
 import ListDetailOther from "../pages/listDetailOther.vue";
+import TimeLine from "../pages/TimeLine.vue";
 const router = createRouter({
     history:createWebHistory(),
     routes:[
